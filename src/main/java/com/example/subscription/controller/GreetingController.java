6f0fd5e11a37419335e706c440d501b82d07356a
Njdos <14njdos@gmail.com>
2021-08-10 18:16:31 +0300

    sdfd

--- src/main/java/com/example/subscription/controller/GreetingController.java
+++ src/main/java/com/example/subscription/controller/GreetingController.java
@@ -2,14 +2,12 @@ package com.example.subscription.controller;
 
 import com.example.subscription.model.User;
 import com.example.subscription.repos.NewsRepo;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class GreetingController {
